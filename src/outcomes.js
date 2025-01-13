4e73a47dd4e0c0b3f3e82678fef603b8a64ee6a1
Kaster23 <francoierace.dev@gmail.com>
2025-01-13 19:33:14 -0300

    Update outcomes.js

--- src/outcomes.js
+++ src/outcomes.js
@@ -1,1 +1,51 @@
 
+ ScrollTrigger.matchMedia({
+
+    // large
+    "(min-width: 992px)": function () {
+      // Marquee on scroll
+
+      gsap.to(".c-scroll-marquee-wrapper", {
+        scrollTrigger: {
+          trigger: ".c-scroll-marquee-wrapper",
+          start: "top 100%",
+          scrub: true,
+        },
+        xPercent: -20,
+        ease: "quart.easeOut",
+      });
+
+    },
+    // medium
+    "(min-width: 767px) and (max-width: 991px)": function () {
+      // Marquee on scroll
+
+      gsap.to(".c-scroll-marquee-wrapper", {
+        scrollTrigger: {
+          trigger: ".c-scroll-marquee-wrapper",
+          start: "top 100%",
+          scrub: true,
+        },
+        xPercent: -20,
+        ease: "quart.easeOut",
+      });
+
+
+    },
+    // small
+    "(max-width: 480px)": function () {
+      // Marquee on scroll
+
+      gsap.to(".c-scroll-marquee-wrapper", {
+        scrollTrigger: {
+          trigger: ".c-scroll-marquee-wrapper",
+          start: "top 100%",
+          scrub: true,
+        },
+        xPercent: -80,
+        ease: "quart.easeOut",
+      });
+
+
+    },
+  })
