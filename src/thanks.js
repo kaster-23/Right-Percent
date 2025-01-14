96fc43f64d89f975d8b65de239feb7927a9063a2
Kaster23 <francoierace.dev@gmail.com>
2025-01-14 18:09:32 -0300

    Update thanks.js

--- src/thanks.js
+++ src/thanks.js
@@ -1,5 +1,36 @@
 function thanks() {
 
+  gsap.fromTo(".c-hero-content-wrapper", {
+    y: '5rem',
+    opacity: 0
+  }, {
+    y: '0rem',
+    duration: 0.8,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: '.c-hero-blog-img-wrapper',
+      start: 'bot 85%',
+      toggleActions: "play none none reverse",
+    }
+  });
+
+  
+  gsap.fromTo(".c-thanks-img-wrapper", {
+    y: '5rem',
+    opacity: 0
+  }, {
+    y: '0rem',
+    duration: 0.8,
+    delay: 0.6,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: '.c-hero-blog-img-wrapper',
+      start: 'bot 85%',
+      toggleActions: "play none none reverse",
+    }
+  });
+
+
 }
 
 export default thanks
